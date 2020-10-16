585d0a0a6d9f2b86a484150be2a1b10dfdaa5e0c
Skelatorian <mylynhle21@gmail.com>
2020-10-16 11:53:26 -0400

    pushing to github so i can pull to macbook

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-  });
+  let results = animals;
+  console.log(req.query)
+  res.json(results);
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
