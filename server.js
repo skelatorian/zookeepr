e5447adb2bfb10bb25faf7762583c3fec2e83d3c
Skelatorian <mylynhle21@gmail.com>
2020-10-15 20:33:18 -0400

    packed in npm expess

---
+++ server.js
@@ -0,0 +1,19 @@
+const { animals } = require('./data/animals');
+const express = require('express');
+const app = express();
+
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+  });
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
+
+/*The first is that the get() method requires two arguments. The first is a string that describes 
+the route the client will have to fetch from. The second is a callback function that will execute 
+every time that route is accessed with a GET request.The second takeaway is that we are 
+using the send() method from the res parameter (short for response) to send the string Hello! 
+to our client.
+*/
+
